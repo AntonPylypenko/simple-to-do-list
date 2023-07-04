ab843e6cef5340d528bc885513b51d7ebe387b08
AntonPylypenko <anton.pylypenko5@gmail.com>
2023-07-04 10:07:55 +0300

    Added basic styling

--- handlers.js
+++ handlers.js
@@ -2,16 +2,24 @@ const tasks = []
 
 function AddItem(){
     const taskItem = document.getElementById('text').value;
-    tasks.push(taskItem);
+    console.log(taskItem)
+    if(taskItem === '') {
+        alert('Please, enter not empty value!')    
+        return
+    };
 
+    tasks.push(taskItem);
     const id = tasks.length;
     let listItem = document.createElement('li');  
     listItem.setAttribute('id', `li-${id}`)  
     var taskNode = document.createTextNode(tasks.length + '. ' + taskItem);
+    
     let check = createCheckbox(id);
+    let btn = createDeleteButton(id);
 
     listItem.appendChild(check);
     listItem.appendChild(taskNode);
+    listItem.appendChild(btn);
 
     document.getElementById('to-do-list').appendChild(listItem);
 }
@@ -26,13 +34,26 @@ function createCheckbox(id){
     return check;
 }
 
+//часто зустрічаю, що назва створюваного елементу пишеться з великої, то така конвенція ?
+function createDeleteButton(id){
+    let btn = document.createElement('button');
+    btn.innerHTML= "Delete";
+    btn.setAttribute('id', `btn-${id}`);
+    btn.setAttribute('onclick', 'deleteTask(id)');
+
+    return btn;
+}
+
+function deleteTask(id){
+    let elem = document.getElementById(id);
+    elem.parentElement.remove();
+}
 
 //спитай чому те ід знаходиться тільки для чексбокса
 function completeTask(id){
     let elem = document.getElementById(id);
-    console.log(elem.checked)
     if (elem.checked) {       
-        elem.parentElement.setAttribute('class', 'completed')
+        elem.parentElement.setAttribute('class', 'checked')
     } else {
         elem.parentElement.setAttribute('class', '')
     }
